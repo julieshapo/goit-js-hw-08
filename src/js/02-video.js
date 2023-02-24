8fedca7f3a65ec3dc1940775fec74314f84724eb
Julie Shapovalenko <jools.shapo@gmail.com>
2023-02-24 12:25:37 +0200

    add task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000))
+
+function onPlayerTimeUpdate(evt) {
+    console.log(evt.seconds)
+    localStorage.setItem("videoplayer-current-time", evt.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
